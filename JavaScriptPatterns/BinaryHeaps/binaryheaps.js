ad8e96bdfb27af6076a38ce046d75d90a2a5e9e6
mstoklos <mstoklos@PCEN33707.cern.ch>
2013-07-15 15:38:45 +0200

    added push and pop function to BinaryHeap Object

--- JavaScriptPatterns/BinaryHeaps/binaryheaps.js
+++ JavaScriptPatterns/BinaryHeaps/binaryheaps.js
@@ -8,3 +8,27 @@ function BinaryHeap(scoreFunction) {
 	this.content = [];
 	this.scoreFunction = scoreFunction;
 }
+
+// making use of prototype 
+BinaryHeap.prototype = {
+		/**
+		 * function adding new element to the end of array
+		 * @param element
+		 */
+		push: function(element) {
+			this.content.push(element);
+			//TODO write following function
+			//this.bubbleUp(this.content.length - 1);
+		},
+		
+		pop: function() {
+			var result = this.content[0];
+			var end = this.content.pop();
+			if(this.content.length > 0){
+				this.content[0] = end;
+				//TODO write following functionality
+				this.sinkDown(0);
+			}
+		},
+		
+}
