2d24f649c9763e28dbbee697f211004307d9c610
mstoklos <mstoklos@PCEN33707.cern.ch>
2013-07-16 16:15:46 +0200

    adding new functions

--- JavaScriptPatterns/ConstructorPattern/constructorPattern.js
+++ JavaScriptPatterns/ConstructorPattern/constructorPattern.js
@@ -1,17 +1,30 @@
 //constructor pattern example
 //remember in javascript everything is an object
-function Vehicle(name) {
+function Vehicle(name, config) {
 	this.name = name;
-	
+	this.config = config;
 	this.toString = function() {
 		return this.name;
 	};
-	
 }
 
-var Car = new Vehicle('BMW');
-console.log(Car.toString());
+var Car = new Vehicle('BMW', {
+	
+	"id": {
+		value: "1",
+		enumerable: true
+	},
+	"model": {
+		value: "320i",
+		enumerable: true
+	},
+});
 
+//extending functionality
+Car.getModel = function() {
+	var model = Car.config.model.value;
+	console.log("this vehicle model is " + model);
+};
 //Usage
-
+console.log(Car.toString());
 
